ba7f9126c2b956f065b9941e72fe1725d46b6283
Tim <github@timgreen.ws>
2023-06-08 14:36:36 +1000

    added custom colors theme for tailwind that matches tgws v2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,17 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			'white': '#ffffff',
+			'black': '#050505',
+			'emerald-300': '#38ef7d',
+			'emerald-500': '#11998e',
+			'zinc-900':'#161616',
+			'purple-900': '#4f165b',
+			'purple-950' : '#631baf',
+		  },
 	},
   safelist: ['dark:!text-white'],
 	plugins: [require('@tailwindcss/typography')],
